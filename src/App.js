a395e72963fd0e91276345a2aa8f81f664688015
Aileen Santos <asantos3026@gmail.com>
2016-09-26 15:24:36 -0700

    tarot input form displays on the page and the input values can be logged

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Row, Navbar, Grid } from 'react-bootstrap';
+import logo from './logo.gif';
 import './App.css';
+import QuestionForm from './components/QuestionForm.js'
+import TarotLayout from './components/TarotLayout.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { question: '', name: '', formSubmitted: false, cards: [] }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#"><img src={logo} className="App-logo" alt="logo" /></a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+
+        <Grid>
+          <Row>
+            <h2>React Tarot App</h2>
+          </Row>
+
+          {this.getChild()}
+        </Grid>
       </div>
-    );
+    )
+  }
+
+  getChild() {
+    if( this.state.formSubmitted ) {
+      return <TarotLayout {...this.state} />
+    } else {
+      return <QuestionForm submitForm={this.questionFormSubmitted.bind(this)} />
+    }
+  }
+
+  questionFormSubmitted(question, name) {
+    this.setState({ question, name, formSubmitted: true })
   }
 }
 
