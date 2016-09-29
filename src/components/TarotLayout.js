f90affb92e9306d1dddc944ceb8cedd3d47f2878
Aileen Santos <asantos3026@gmail.com>
2016-09-29 16:06:39 -0700

    hey kind of works party people

--- src/components/TarotLayout.js
+++ src/components/TarotLayout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import DescriptionContainer from './DescriptionContainer'
+import TarotModal from './TarotModal'
 import TarotCard from './TarotCard'
 
 export default class TarotLayout extends Component {
@@ -9,23 +11,55 @@ export default class TarotLayout extends Component {
     this.state = { currentIndex: 0 }
   }
 
+  tarotCardClickHandler() { 
+    this.showNextCard() 
+    this.showTarotModal()    
+  }
+
   showNextCard() {
     const { flipCard } = this.props 
 
     flipCard( this.state.currentIndex )
-
     this.setState({ currentIndex: this.state.currentIndex + 1 })
+  }
+
+  showTarotModal() {
+    this.setState({ showTarotModal: true })
+  }
+
+  hideTarotModal() {
+    this.setState({ showTarotModal: false })
+  }
+
+  getTarotModal() {
+    const { cards } = this.props
+    const displayCard = cards.slice( this.state.currentIndex - 1, this.state.currentIndex )
+
+    if( this.state.showTarotModal ) {
+      return ( 
+        <TarotModal hideModal={this.hideTarotModal.bind(this)}>
+          <DescriptionContainer cards={displayCard} />
+        </TarotModal>
+      ) 
+    } else {
+      return <div></div>
+    }
   }
 
   render() {
     const { cards } = this.props
-
     return (
-      <div className='tarot-board' onClick={this.showNextCard.bind(this)}>
+      <div className='tarot-board' onClick={this.tarotCardClickHandler.bind(this)}>
         {cards.map( (card, index) => 
           <TarotCard key={`tarot-card-${index}`} {...card} cardIndex={index} /> 
         )}
+        {this.getTarotModal()}
       </div>
     )
   }
 }
+
+
+
+ 
+
